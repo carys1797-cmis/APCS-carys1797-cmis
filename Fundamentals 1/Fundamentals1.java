9786fd88158b34a12e6fb17e8e32f357a56ad39e
Carys Owen <carys1797@cmis.ac.th>
2018-08-15 11:55:44 +0700

    Worked on Fundamentals 1

--- Fundamentals 1/Fundamentals1.java
+++ Fundamentals 1/Fundamentals1.java
@@ -3,8 +3,26 @@ public class Fundamentals1
     public static void main ( String args[] )
         {
          System.out.println("");  
-            
+         System.out.println("Number of students in 7th grade:" + totalstudents7th); 
         } // end method main
-        
-        
+    
+    public static void dataTypes() {
+        byte ; // number of students in a CMIS class
+                //precentage on a test (0dp)
+                //
+        short ; // number of skips someone can do in a minute,
+        int ;
+        long ;
+        float ;
+        double ;
+        boolean ;//
+        char ;
+    }
+
+    public static int add(int a, int b) {
+        int a = 24;
+        int b = 19;
+        totalstudents7th = a + b;
+    }
+   
 } // end class Fundamentals1
