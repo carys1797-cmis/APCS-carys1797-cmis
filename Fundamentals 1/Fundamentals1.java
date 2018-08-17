bebd082b8812d90b10a5910234b0519557065867
Carys Owen <carys1797@cmis.ac.th>
2018-08-18 01:45:59 +0700

    Worked on Fundamentals1 problems

--- Fundamentals 1/Fundamentals1.java
+++ Fundamentals 1/Fundamentals1.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 public class Fundamentals1
-{
+{   
     public static int add(int a, int b) {
         int sum = a + b;
         return sum;
@@ -39,9 +41,64 @@ public class Fundamentals1
         double product = a / b;
         return product;
     }
-        
+    
+    private static final int a = 729;
+    private static final int b = 35;
+    private static final double c = 92.43;
+    private static final double d = 3402.1;
+   
+    public static void compare(int a, int b){
+        if (a > b){
+            System.out.println("The value of a is " + a + " and it is greater than b whose value is " + b + ".");
+        } else if (a < b){
+            System.out.println("The value of a is " + a + " and it is less than b whose value is " + b + ".");
+        } else {
+            System.out.println("The value of a is " + a + " and it is equal to b whose value is " + b + ".");
+        }
+    }
+    
+    public static int evenOddZero(int a) {
+        if (a == 0) {
+            return 0;}
+        else {
+            int evo = a % 2;
+            if (evo > 0) {
+                return -1;}
+            else {
+            return 1;}
+        }
+    }
+    
     public static void main ( String args[] )
         {
-         System.out.println("Number of students in 7th grade: " + add(19, 24));
+          System.out.println(a + " + " + b + " = " + add(a, b));
+          System.out.println(a + " x " + b + " = " + mul(a, b));
+          System.out.println(c + " + " + d + " = " + addd(c, d));
+          System.out.println(c + " x " + d + " = " + muld(c, d));
+          
+          Scanner reader = new Scanner(System.in);
+          System.out.println("Enter an integer: ");
+          int w = reader.nextInt();
+          System.out.println("Enter another integer: ");
+          int x = reader.nextInt();
+          System.out.println(w + " - " + x + " = " + sub(w, x));
+          System.out.println(w + " / " + x + " = " + div(w, x));
+          
+          System.out.println("Enter a decimal: ");
+          double y = reader.nextDouble();
+          System.out.println("Enter another decimal: ");
+          double z = reader.nextDouble();
+          System.out.println(y + " - " + z + " = " + subd(y, z));
+          System.out.println(y + " / " + z + " = " + divd(y, z));
+        
+          System.out.println("Enter an integer: ");
+          int comp1 = reader.nextInt();
+          System.out.println("Enter another integer: ");
+          int comp2 = reader.nextInt();
+          compare(comp1, comp2);
+          
+          System.out.println("Enter another integer: ");
+          int evo = reader.nextInt();
+          System.out.println(evenOddZero(evo));
         } // end method main
 } // end class Fundamentals1
