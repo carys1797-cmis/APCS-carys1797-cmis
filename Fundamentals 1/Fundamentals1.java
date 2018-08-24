f1f588e7d924aff559b2518d1f4f646d02b7a27b
Carys Owen <carys1797@cmis.ac.th>
2018-08-24 09:58:44 +0700

    Finished Fundamentals1

--- Fundamentals 1/Fundamentals1.java
+++ Fundamentals 1/Fundamentals1.java
@@ -5,23 +5,30 @@ import java.util.Random;
 public class Fundamentals1
 {   
     public static void dataTypes() {
-        String abyte = "number of students in a CMIS class, day of the month, change of rank of song within top 100";
+        String abyte = "A byte is an 8-bit integer. With a minimum value of -128 and a maximum value of 127, it is the smallest data type. Some situations in which the byte data type might be used is for number of students in CMIS class at CMIS, the day of the month, or the change of rank of song within top 100.";
         byte numstu = 10; //number of students in AP Computer Science
-        String ashort = "number of skips someone can do in a minute, hourly change in people in stadium, person's weight in pounds";
+        System.out.println(abyte + numstu);
+        String ashort = "Short data type is a 16-bit integer. It has a minimum value of -32,768 and a maximum value of 32,767. Some examples of when this data type might be used in the real world is for the number of skips someone can do in a minute, the hourly change in people in stadium, or perhaps a person's weight in pounds.";
         short numskp = 332; //Guiness World record for skips in a minute
-        String anint = "population of a country, daily change in library books loaned, number of active users on facebook at any given time";
+        System.out.println(ashort + numskp);
+        String anint = "An integer is a 32-bit integer. It is the default data type with a minimum value of -2,147,483,648 and a maximum value of 2,147,483,647. In the real world, an integer could be used for the population of a country, the daily change in library books loaned, or the number of active users on facebook at any given time.";
         int chngbk = -8345000; //change in library books loaned at the Library of Congress
-        String along = "yearly increase/decrease of stars being born, grains of sand on a beach, the number of bytes transferred over the internet per day";
+        System.out.println(anint + chngbk);
+        String along = "The long data type is a 64-bit integer. It has a min value of -9,223,372,036,854,775,808 and a max value of 9,223,372,036,854,775,807. Some situations in which this data type might be used is for the yearly increase/decrease of stars being born, the grains of sand on a beach, the number of bytes transferred over the internet per day.";
         long grains = 7500000000000000000L;
-        String afloat = "feet above sealevel, temperature, percentage on a test";
+        System.out.println(along + grains);
+        String afloat = "The float data type is a 32-bit floating point. This means that it can hold decimal values. This data type could be used for the height of a place above sealevel (in feet), the temperature, or a student's percentage on a test.";
         float abovesea = -282.0f; //height of Death Valley above sea level (in feet)
-        //String adouble = "";
-        //double;
-        String aboolean = "It is raining, temperature is too high (for medicines), oxygen levels are too low (on an airplane)";
+        System.out.println(afloat + abovesea);
+        String adouble = "The double data type is a 64-bit floating point. This is the default data type for decimal values. The double data type can be used for extremely precise numbers such as the measurement of ingredients when making medicine, the landing coordinates for a spaceship, or even the size of an atom.";
+        double hydroatom = 0.000000000053; //the size of a hydrogen atom
+        System.out.println(adouble + hydroatom);
+        String aboolean = "The boolean data type represents one bit of information. There are only two possible values: true and false. This data type could be used to determine the validity of the following statements: it is raining; the temperature is too high; oxygen levels are too low.";
         boolean rain = false; //it is raining
-        String achar = "first letter of someone's name, grade in a class, type of money (symbol)";
+        System.out.println(aboolean + rain);
+        String achar = "The char data type is a 16-bit Unicode character. It can store any of 65,535 characters in the unicode \"dictionary\". It can be used for the first letter of someone's name, for a student's grade in a class, to describe the type of money (the symbol).";
         char flname = 'c'; //first letter of my name
-        //System.out.println(numstu, numskp, chngbk, grains, abovesea, rain, flname);
+        System.out.println(achar + flname);
     }
     public static int add(int a, int b) {
         int sum = a + b;
@@ -127,16 +134,23 @@ public class Fundamentals1
         for(int j = h; j > 0; j--){
             body = body + "|";
             if(j % 2 == 1){
-                body = body + "#";
-                for(int i = (w - 2); i > 0; i--){
-                    body = body +" #";
+                for(int i = w; i > 0; i--){
+                    if (i % 2 == 0){
+                        body = body + "#";
+                    }else {
+                        body = body + " ";
+                    }
                 }
                 body = body + "|" + "\n";
             }else {
-                for(int i = (w - 2); i > 0; i--){
-                    body = body + " #";
+                for(int i = w; i > 0; i--){
+                    if (i % 2 == 0){
+                        body = body + " ";
+                    }else {
+                        body = body + "#";
+                    }
                 }
-                body = body + " |" + "\n";
+                body = body + "|" + "\n";
             }
         }
         String lastline = "+";
@@ -157,6 +171,7 @@ public class Fundamentals1
     
     public static void main ( String args[] )
         {
+          dataTypes();
           System.out.println(a + " + " + b + " = " + add(a, b));
           System.out.println(a + " x " + b + " = " + mul(a, b));
           System.out.println(c + " + " + d + " = " + add(c, d));
