56180b66befbd0680c1bc01e2d96df9542fe4fe0
Carys Owen <carys1797@cmis.ac.th>
2018-08-31 12:07:53 +0700

    Worked on BinaryConverter

--- Fundamentals 1/BinaryConverter.java
+++ Fundamentals 1/BinaryConverter.java
@@ -2,20 +2,42 @@ public class BinaryConverter
 {
     public static void main(String args[]){
         System.out.println("BinaryConverter");
-        //System.out.println(bin2dec("00110010101011001101001101001010"));
-        System.out.println(dec2bin(52));
+        System.out.println("10110010101011001101001101001010 = " + bin2dec("10110010101011001101001101001010"));
+        System.out.println("52 = " + dec2bin(52));
+        String ya = "ya";
+        System.out.println(ya.length());
     }
     
     public static int bin2dec(String bin){
-        String bin2 = "0b" + bin;
-        //int bin2;
-        int asint = Integer.valueOf(bin2).intValue();
+        int asint = Integer.parseInt(bin, 2);
         
         /*
          * Takes a String that looks like a 32-bit binary number and returns
          * the equivalent int value
          */
         return asint;
+    }
+    
+    public static int trying (String bin) {
+        int asint = 0;
+        int len = bin.length();
+        if (bin.charAt(0) == '0'){
+            for (int i = 1; i < len; i++){
+                int p = len - i;
+                if (bin.charAt(p) == '1'){
+                    asint += 2 ^ i;
+                }else {
+                    continue;
+                }
+            }
+            
+        }else {
+            if (bin.charAt(-1) == '1'){
+                bin = bin.substring(0, len-1) + "0";
+            }
+            
+        }
+        return 1;
     }
     
     public static String dec2bin(int dec){
