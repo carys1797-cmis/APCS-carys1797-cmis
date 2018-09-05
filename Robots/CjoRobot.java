d605bb75b82b754c306aeaa352fb17ae356488ad
Carys Owen <carys1797@cmis.ac.th>
2018-09-05 09:58:30 +0700

    Worked on CjoRobot

---
+++ Robots/CjoRobot.java
@@ -0,0 +1,43 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class CjoRobot extends Robot
+{
+    public CjoRobot(){
+        super(Color.RED);
+    }
+    
+    public void init(){
+        
+    }
+    
+    /**
+     * What your robot can do:
+     * right() => move 1 to the right
+     * left() => move 1 to the left
+     * up() => move 1 up
+     * down() => move 1 down
+     * isClearRight() => true means no block to the right
+     * isClearLeft() => true means no block to the left
+     * isClearUp() => true means no block up
+     * isClearDown() => true means no block down
+     */
+    public void behave(){
+        if (isClearRight() == true){
+            right();
+        } else if (isClearRight() == false) {
+            if (isClearUp() == true) {
+                up();
+                if (isClearRight() == false) {
+                    left();
+                }
+            }else if (isClearDown() == true){
+                down();
+                if (isClearRight() == false) {
+                    left();
+                }
+            } else if (isClearLeft() == true){
+                left();
+            }
+        }
+    }
+}
