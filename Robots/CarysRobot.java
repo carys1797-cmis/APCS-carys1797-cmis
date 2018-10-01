54abc284cb97f8eda12286552a9732c947bddadc
Carys Owen <carys1797@cmis.ac.th>
2018-10-01 11:56:51 +0700

    Worked on Robots

--- Robots/CarysRobot.java
+++ Robots/CarysRobot.java
@@ -22,26 +22,16 @@ public class CarysRobot extends Robot
      * isClearDown() => true means no block down
      */
     public void behave(){
-        Random rand = new Random();
-        int r = rand.nextInt(50);
-        //move left until up is clear or down is clear
-        if (isClearRight() == true && r < 40){
+        int r = (int) (Math.random() * 100) + 1;
+        if (isClearRight() == true && r < 75){
             right();
-        }else if (isClearUp() == true || isClearDown() == true){
-            if (r < 25){
-                if (isClearUp() == true){
-                    up();
-                }else if (isClearDown() == true){
-                    down();
-                }
-            }else if (r >= 25){
-                if (isClearDown() == true){
-                    down();
-                }else if (isClearUp() == true){
-                    up();
-                }
-            }
+        }else if (isClearUp() == true && r >= 75 && r < 85){
+            up();
+        }else if (isClearDown() == true && r >= 85 && r < 95){
+            down();
+        }else if (isClearLeft() == true && r >= 95){
+            left();
         }
-        left();
+        
     }
 }
