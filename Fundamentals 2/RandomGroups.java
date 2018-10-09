057649dfcdf2ca07f1e5cccea5d3205753a5ec03
Carys Owen <carys1797@cmis.ac.th>
2018-10-09 14:05:58 +0700

    makeGroups problem

--- Fundamentals 2/RandomGroups.java
+++ Fundamentals 2/RandomGroups.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 public class RandomGroups
 {
     public static void main(String args[]){
-        String[] ya = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"};
-        makeGroups(ya, 6);
+        String[] ya = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q"};
+        makeGroups(ya, 3);
     }
 
     public static String[] makeGroups(String[] names, int groupSize){
@@ -12,11 +12,10 @@ public class RandomGroups
             int r;
             do {
                 r = (int) (Math.random() * names.length);
-            }while (names[r] == "");
+            }while (names[r] == null);
             shuffle[i] = names[r];
-            names[r] = "";
+            names[r] = null;
         }
-        System.out.println(Arrays.toString(shuffle));
         int n = names.length / groupSize;
         String[] groups = new String[n];
         for (int i = 0; i < n; i++){
@@ -26,13 +25,14 @@ public class RandomGroups
                 groups[i] += " ";
             }
         }
-        System.out.println(Arrays.toString(groups));
         int rem = names.length % groupSize;
         do{
             int u = shuffle.length - rem;
             for (int i = 0; i < n; i++){
-                groups[i] += shuffle[u + i];
-                groups[i] += " ";
+                if (u + i < shuffle.length){
+                    groups[i] += shuffle[u + i];
+                    groups[i] += " ";
+                }
             }
             rem -= n;
         } while (rem > 0);
