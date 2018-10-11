1df5747425673281544461f80738d019719f8603
Carys Owen <carys1797@cmis.ac.th>
2018-10-11 09:57:23 +0700

    Worked on Robots

--- Robots/CarysRobot.java
+++ Robots/CarysRobot.java
@@ -22,6 +22,41 @@ public class CarysRobot extends Robot
      * isClearDown() => true means no block down
      */
     public void behave(){
-        right();
+        int i = 0;
+        if (isClearRight() == true){
+            right();
+            while (getData(i) != 0){
+                i++;
+                if (i == 10){
+                    i = 0;
+                }
+            }
+            setData(i, 1); //1 means right
+        }else if (isClearUp() == true){
+            for (int j = 0; j < 9; j++){
+                if (getData(j) == 2 && getData(j+1) == 3){
+                    down();
+                }
+            }
+            up();
+            while (getData(i) != 0){
+                i++;
+                if (i == 10){
+                    i = 0;
+                }
+            }
+            setData(i, 2); //2 means up
+        }else if (isClearDown() == true){
+            down();
+            while (getData(i) != 0){
+                i++;
+                if (i == 10){
+                    i = 0;
+                }
+            }
+            setData(i, 3); //3 means up
+        }else {
+            left();
+        }
     }
 }
