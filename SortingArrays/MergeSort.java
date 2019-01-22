ba744545b0e12bc18c97de0acb713efece5969ee
Carys Owen <carys1797@cmis.ac.th>
2019-01-23 01:05:39 +0700

    Worked on MergeSort

--- SortingArrays/MergeSort.java
+++ SortingArrays/MergeSort.java
@@ -1,7 +1,7 @@
 
 public class MergeSort
 {
-    public static void main(String[] args){
+    public static void main(String args[]){
         int[] numbers = new int[15];
         for(int i = 0; i < numbers.length; i++){
             numbers[i] = (int)(Math.random() * 100);
@@ -21,6 +21,9 @@ public class MergeSort
     public static void mergeSort(int[] array){
         int len = array.length;
         int[] arrayFront = new int[len / 2];
+        int[] result = new int[len];
+        boolean sortedf = false;
+        boolean sortedb = false;
         for(int i = 0; i < arrayFront.length; i++){
             arrayFront[i] = array[i];
         }
@@ -31,21 +34,36 @@ public class MergeSort
         //print(arrayFront);
         //print(arrayBack);
         if (arrayFront.length == 1){
-            System.out.println("Split");
             if(arrayBack.length == 1){
-                System.out.println("Split");
-                print(arrayFront);
-                print(arrayBack);
+                int combinedlen = arrayFront.length + arrayBack.length;
+                int f = 0;
+                int b = 0;
+                for(int i = 0; i < combinedlen; i++){
+                    if(f == arrayFront.length){
+                        result[i] = arrayBack[b];
+                        b++;
+                    }else if(b == arrayBack.length){
+                        result[i] = arrayFront[f];
+                        f++;
+                    }else if(arrayFront[f] < arrayBack[b]){
+                        result[i] = arrayFront[f];
+                        f++;
+                    }else if(arrayBack[b] < arrayFront[f]){
+                        result[i] = arrayBack[b];
+                        b++;
+                    }
+                }
+                print(result);
             }else{
-                print(arrayBack);
                 mergeSort(arrayBack);
             }
         }else{
-            print(arrayFront);
             mergeSort(arrayFront);
         }
+        print(result);
     }
     //split
     //mergeSort
-    //merge
+    //merge:
+        //first of front, first of back
 }
