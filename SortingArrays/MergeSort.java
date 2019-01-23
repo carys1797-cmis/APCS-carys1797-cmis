b4fb80e3ab6673372cba0743e222d5df4f392f2a
Carys Owen <carys1797@cmis.ac.th>
2019-01-23 14:35:19 +0700

    Made Progress on MergeSort

--- SortingArrays/MergeSort.java
+++ SortingArrays/MergeSort.java
@@ -10,60 +10,54 @@ public class MergeSort
         mergeSort(numbers);
         //print(numbers);
     }
-    
+
     public static void print(int[] array){
         for(int i : array){
             System.out.print(i + " ");
         }
         System.out.println();
     }
-    
-    public static void mergeSort(int[] array){
-        int len = array.length;
-        int[] arrayFront = new int[len / 2];
-        int[] result = new int[len];
-        boolean sortedf = false;
-        boolean sortedb = false;
-        for(int i = 0; i < arrayFront.length; i++){
-            arrayFront[i] = array[i];
-        }
-        int[] arrayBack = new int[(len + 1) / 2];
-        for (int i = 0; i < arrayBack.length; i++){
-            arrayBack[i] = array[i + arrayFront.length];
-        }
-        //print(arrayFront);
-        //print(arrayBack);
-        if (arrayFront.length == 1){
-            if(arrayBack.length == 1){
-                int combinedlen = arrayFront.length + arrayBack.length;
-                int f = 0;
-                int b = 0;
-                for(int i = 0; i < combinedlen; i++){
-                    if(f == arrayFront.length){
-                        result[i] = arrayBack[b];
-                        b++;
-                    }else if(b == arrayBack.length){
-                        result[i] = arrayFront[f];
-                        f++;
-                    }else if(arrayFront[f] < arrayBack[b]){
-                        result[i] = arrayFront[f];
-                        f++;
-                    }else if(arrayBack[b] < arrayFront[f]){
-                        result[i] = arrayBack[b];
-                        b++;
-                    }
-                }
-                print(result);
-            }else{
-                mergeSort(arrayBack);
-            }
+
+    public static int[] mergeSort(int[] array){
+        if (array.length <= 1){
+            return array;
         }else{
+            int len = array.length;
+            int[] result = new int[len];
+            int[] arrayFront = new int[len / 2];
+            for(int i = 0; i < arrayFront.length; i++){
+                arrayFront[i] = array[i];
+            }
+            int[] arrayBack = new int[(len + 1) / 2];
+            for (int i = 0; i < arrayBack.length; i++){
+                arrayBack[i] = array[i + arrayFront.length];
+            }
             mergeSort(arrayFront);
+            mergeSort(arrayBack);
+            int combinedlen = arrayFront.length + arrayBack.length;
+            int f = 0;
+            int b = 0;
+            for(int i = 0; i < combinedlen; i++){
+                if(f == arrayFront.length){
+                    array[i] = arrayBack[b];
+                    b++;
+                }else if(b == arrayBack.length){
+                    array[i] = arrayFront[f];
+                    f++;
+                }else if(arrayFront[f] < arrayBack[b]){
+                    array[i] = arrayFront[f];
+                    f++;
+                }else if(arrayBack[b] < arrayFront[f]){
+                    array[i] = arrayBack[b];
+                    b++;
+                }
+            }
         }
-        print(result);
+        print(array);
+        return array;
     }
     //split
     //mergeSort
     //merge:
-        //first of front, first of back
+    //first of front, first of back
 }
