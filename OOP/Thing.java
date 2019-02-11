4544a82e7f8778f95ac4214779e484882e7b141a
Carys Owen <carys1797@cmis.ac.th>
2019-02-11 14:37:56 +0700

    Object Oriented Programming

---
+++ OOP/Thing.java
@@ -0,0 +1,43 @@
+
+public class Thing
+{
+    private int field1 = 52;
+    private double field2 = 3.74;
+    private boolean field3 = false;
+    private String field4 = "field4";
+    
+    public Thing(){
+        this.field1 = 10;
+        this.field2 = 10.10;
+        this.field3 = true;
+        this.field4 = "ten";
+    }
+
+    public void setfield1(int newValue){
+         this.field1 = newValue;
+    }
+    public int getfield1(){
+        return field1;
+    }
+    
+    public void setfield2(double newValue){
+         this.field2 = newValue;
+    }
+    public double getfield2(){
+        return field2;
+    }
+    
+    public void setfield3(boolean newValue){
+         this.field3 = newValue;
+    }
+    public boolean getfield3(){
+        return field3;
+    }
+    
+    public void setfield4(String newValue){
+         this.field4 = newValue;
+    }
+    public String getfield4(){
+        return field4;
+    }
+}
