67ed9fe20bf3a5ff0a7e647b888bcd360592dd92
Carys Owen <carys1797@cmis.ac.th>
2019-02-22 11:56:42 +0700

    Added a user interface

--- OOP/ClockApplication.java
+++ OOP/ClockApplication.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
 
 public class ClockApplication
 {
     public static void main (String arg[]){
         Clock kitchenClock = new Clock();
         System.out.println(kitchenClock);
-        Clock bedroomClock = new Clock(14, 12, 52);
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Hours: ");
+        int hrs = reader.nextInt();
+        System.out.println("Minutes: ");
+        int mins = reader.nextInt();
+        System.out.println("Seconds: ");
+        int secs = reader.nextInt();
+        Clock bedroomClock = new Clock(hrs, mins, secs);
         System.out.println(bedroomClock);
     }
 }
